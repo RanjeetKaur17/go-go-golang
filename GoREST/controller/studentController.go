7f65b289ce402567943f1c5918afadc8b545ea45
RanjeetKaur17 <ranjeet@algorythma.com>
2018-07-12 11:40:40 +0400

    Tutorial 8: RESTfully Exposing Services in Go.

--- GoREST/controller/studentController.go
+++ GoREST/controller/studentController.go
@@ -2,7 +2,7 @@ package controller
 
 import (
 	"go-go-golang/GoREST/model"
-	"go-go-golang/GoREST/dao"
+	"go-go-golang/GoREST/service"
 	"github.com/gin-gonic/gin"
 	"strconv"
 )
@@ -10,12 +10,10 @@ import (
 func AddStudent(c *gin.Context){
 	student := model.Student{}
 	err := c.BindJSON(&student)
-	rowsAffected, lastInsertedId, err := dao.AddStudent(student)
+	student, err = service.AddStudent(student.Name, student.Age)
 	exception := ""
-	if err == nil && rowsAffected > 0 {
-		student.StudentID = lastInsertedId
-	} else if err == nil {
-		exception = "Some Error Occured"
+	if err != nil {
+		exception = err.Error()
 	}
 	c.JSON(200, gin.H{"exception": exception, "data": student})
 }
@@ -23,33 +21,30 @@ func AddStudent(c *gin.Context){
 func UpdateStudent(c *gin.Context){
 	student := model.Student{}
 	err := c.BindJSON(&student)
-	rowsAffected, lastInsertedId, err := dao.AddStudent(student)
+	student, err = service.UpdateStudent(student.StudentID, student.Name,  student.Age)
 	exception := ""
-	if err == nil && rowsAffected > 0 {
-		student.StudentID = lastInsertedId
-	} else if err == nil {
-		exception = "Some Error Occured"
+	if err != nil {
+		exception = err.Error()
 	}
 	c.JSON(200, gin.H{"exception": exception, "data": student})
 }
 
 func DeleteStudent(c *gin.Context){
 	studentID, _ := strconv.ParseInt(c.Params.ByName("studentID"), 10, 64)
-	rowsAffected, err := dao.DeleteStudent(studentID)
+	isDeleted, err := service.DeleteStudent(studentID)
 	exception := ""
-	if err == nil && rowsAffected == 0 {
-		exception = "No Data Found"
-	} else if err == nil {
-		exception = "Some Error Occured"
-	} else if err != nil{
+	if err != nil {
 		exception = err.Error()
+	}
+	if !isDeleted {
+		exception = "Student Could not be deleted"
 	}
 	c.JSON(200, gin.H{"exception": exception})
 }
 
 func GetStudent(c *gin.Context){
 	studentID, _ := strconv.ParseInt(c.Params.ByName("studentID"), 10, 64)
-	student, err := dao.GetStudent(studentID)
+	student, err := service.GetStudent(studentID)
 	exception := ""
 	if err != nil{
 		exception = err.Error()
